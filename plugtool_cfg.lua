d98ce2c03fa30418b97630908723cf63dd5d7d2f
Cedric Mamo <vrighter@gmail.com>
2022-03-03 18:40:39 +0100

    bugfix

--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -1,6 +1,5 @@
 return {
-	needs = { "vrighter/pixel.nvim" },
-	after = { "vrighter/pixel.nvim" },
+	needs = { "vrighter/meow.nvim" },
 	config = function()
 		vim.o.termguicolors = true
 		nnoremap("<leader>mario", ':lua require"mario".its_a_meee()<cr>', "silent", "Pixel: toggle little buddies")
